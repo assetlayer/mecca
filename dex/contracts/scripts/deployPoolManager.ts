4419cf54c92ba6ed6f145bc22e54218e145c4cd9
AssetLayer <assetlayerorg@gmail.com>
2025-10-20 15:13:30 +0400

    Fix Hardhat TypeScript loader configuration

--- dex/contracts/scripts/deployPoolManager.ts
+++ dex/contracts/scripts/deployPoolManager.ts
@@ -2,10 +2,6 @@ import { ethers } from "hardhat";
 import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
